9c1f5dbf2d3a05dac924e012471a4b043e0c5e58
TrebbleBiscuit <TrebbleBiscuit@gmail.com>
2015-06-07 15:16:48 -0400

    Automated Visited
    visited(area) will now return True or False depending on if
    visited(area) was previously called.

--- visited.py
+++ visited.py
@@ -1,14 +1,15 @@
 visitedareas = {}
 
-def visited():
+def visited(area):
     try:
-        if visitedareas['tavern']:  # If 'tavern' is in 'visitedareas' dictionary
+        if visitedareas[area]:  # If 'tavern' is in 'visitedareas' dictionary
             pass
         return True
     except KeyError:  # If 'tavern' is not in 'visitedareas' dictionary
-        visitedareas['tavern'] = True
+        visitedareas[area] = True
         return False
 
-print(visited())
-print(visited())
-print(visited())
+print(visited("Tavern"))  # False
+print(visited("Tavern"))  # True
+print(visited("Blacksmith"))  # False
+print(visited("Tavern"))  # True
