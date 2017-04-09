89271f51381fbbb17623033667d3199384f030c7
TrebbleBiscuit <erik@dankmemes.local>
2017-04-09 16:58:00 -0400

    Change RockPaperScissors into a class
    Allows RPSGame to be imported and used in the game.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,39 +1,38 @@
 from random import randint
 
-def choicename(int):
-    if int == 1:
-        return "rock"
-    elif int == 2:
-        return "paper"
-    elif int == 3:
-        return "scissors"
+class RPSGame():
 
-def guess():
-    guess = int(raw_input("play 1 for rock, 2 for paper, and 3 for "
-                          "scissors. "))
-    while guess != 1 and guess != 2 and guess != 3:
-        guess = int(raw_input("Either pick 1 (rock,) 2 (paper,) "
-                              "or 3 (scissors.) "))
-    print("You chose %s!" % choicename(guess))
-    return guess
+    def choicename(self, int):
+        if int == 1:
+            return "rock"
+        elif int == 2:
+            return "paper"
+        elif int == 3:
+            return "scissors"
 
-def aiguess():
-    opponent = randint(1,3)
-    print("I picked %s." % choicename(opponent))
-    return opponent
+    def guess(self):
+        guess = int(raw_input("play 1 for rock, 2 for paper, and 3 for "
+                              "scissors. "))
+        while guess != 1 and guess != 2 and guess != 3:
+            guess = int(raw_input("Either pick 1 (rock,) 2 (paper,) "
+                                  "or 3 (scissors.) "))
+        print("You chose %s!" % self.choicename(guess))
+        return guess
 
-def game():
-    print("Let's play rock paper scissors!")
-    choice = guess()
-    opchoice = aiguess()
-    if opchoice == choice - 1 or opchoice == choice + 2:
-        print ("{} beats {}! You win!").format(choicename(choice),
-                                               choicename(opchoice))
-    elif opchoice == choice + 1 or opchoice == choice - 2:
-        print ("{} beats {}! You lost!").format(choicename(opchoice)
-                                                 .title(),
-                                                 choicename(choice))
-    elif opchoice == choice:
-        print("We tied!")
+    def aiguess(self):
+        opponent = randint(1,3)
+        print("I picked %s." % self.choicename(opponent))
+        return opponent
 
-game()
+    def game(self):
+        choice = self.guess()
+        opchoice = self.aiguess()
+        if opchoice == choice - 1 or opchoice == choice + 2:
+            print ("{} beats {}! You win!").format(self.choicename(choice),
+                                                   self.choicename(opchoice))
+        elif opchoice == choice + 1 or opchoice == choice - 2:
+            print ("{} beats {}! You lost!").format(self.choicename(opchoice)
+                                                     .title(),
+                                                     self.choicename(choice))
+        elif opchoice == choice:
+            print("We tied!")
