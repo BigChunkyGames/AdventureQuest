cfed9b27cde4d09679ff001643063cbf5d321a79
TrebbleBiscuit <erik@dankmemes.local>
2018-11-18 17:29:53 -0500

    Added Lists

--- game.py
+++ game.py
@@ -9,8 +9,9 @@ from places.maintown import *
 clear() 
 print("Welcome to ADVENTURE QUEST Version 0.00.42P! The P stands for python.")
 
-#TODO: assuming new game each time. Should ask to load saved game data or start 
-# new game. Below line creates a new player object but should load if loading saved game
+# TODO: Ask to load saved game data or start new game
+
+# if newgame:
 player = Player() #make new player object in player.py
 
 
@@ -23,16 +24,20 @@ debug = 1
 def main():
     #intro()
     player.charcreation() 
-    introduction(player)
-    maintown(player)
+    introduction(player)    # intro.py
+    maintown(player)        # places
 
 if debug == 1:
     player.aspect['name'] = "name"
     player.aspect['gender'] = "boi"
-    player.aspect['heshe'], player.aspect['HeShe'], player.aspect['hisher'] = "he", "He", "his"
-    player.aspect['occ'], player.aspect['viverb'], player.aspect['skill1'], player.aspect['skill2'] = "fireman", "stab", "sewing", "rubiks cube solving"
+    player.aspect['heshe'], player.aspect['HeShe'], player.aspect['hisher'] = \
+        "he", "He", "his"
+    player.aspect['occ'], player.aspect['viverb'], player.aspect['skill1'], \
+    player.aspect['skill2'] = "fireman", "stab", "sewing", "rubiks cube solving"
     player.aspect['town'], player.aspect['hills'] = "Swagsburgh", "Peak's Hills"
-    player.aspect['adj1'], player.aspect['adj2'], player.aspect['adj3'], player.aspect['adj4'], player.aspect['adj5'] = "cool", "neato", "sick nasty", "wiggity wiggity whack", "excellent"
+    player.aspect['adj1'], player.aspect['adj2'], player.aspect['adj3'], \
+    player.aspect['adj4'], player.aspect['adj5'] = \
+        "cool", "neato", "sick nasty", "wiggity wiggity whack", "excellent"
     maintown(player)
 else:
     main()
--- lists.py
+++ lists.py
@@ -1,5 +1,23 @@
 # The purpose of this file is to hold lists like a list of all dank memes
 
+import random
+
 def getRandomDankAdjective():
-    adjs = ["Dank", "Sick", "MLG", "Nice", "Dope", "Swiggity" ]
+    adjs = ["Dank", "Sick", "MLG", "Nice", "Dope", "Swiggity"]
     return adjs[random.randint(0, len(adjs)-1)]
+
+def getWeaponPrefix
+
+def getRandomMemePeople():
+    names = ["Caillou", "Gabe Newell", "Batman", "Sanic", "Peppa Pig", "Pepe", "Famous Basketball Player Shaquille O'Neal", "Big Man Tyrone", "Voiceover Pete", "Shrek", "Neil deGrasse Tyson", "Bill Nye the Science Guy", "xXx_Sinpars_xXx", "Nicholas Cage", "Sergei Rachmaninoff"]
+    return names[random.randint(0, len(names)-1)]
+
+def getReaction(reactionlevel):
+    reaction = []
+    if reactionlevel == 1:
+        reaction = ["ehh", "pff"]
+    elif reactionlevel == 2:
+        reaction = ["oh cool", "neat"]
+    elif reactionlevel == 3:
+        reaction = ["whoa dude", "that was sick"]
+    return reaction[random.randint(0, len(reaction)-1)]
